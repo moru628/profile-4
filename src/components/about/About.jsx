d30a8259145222fd003e543d18db61eb33a99bb9
moru628 <WANGMO@tcd.ie>
2024-09-12 13:09:01 +0100

    Describe your changes

---
+++ src/components/about/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './about.css'
+import CV from '../../assets/Moru_CV.pdf'
+import Info from './info'
+
+const About = () => {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section_title">About Me</h2>
+        <span className="section_subtitle">My introduction</span>
+
+        <div className="about_container container grid">
+            <div className="about_data">
+                <Info />
+                <p className="about_description">
+                    I am a graduated student of Trinity College Dublin with strong passion on UI/UX design. I've worked on many group projects during my studing. I always rely on surveys to understand the user requirements and translate them into effective web and mobile interface designs. I am a quick learner and outgoing communicator, focused on creating impactful designs that meet user's needs.
+                </p>
+
+                <div className="about_button_container">
+                    <a download="" href={CV} className="button button--flex about_button">
+                        Download CV
+                        <i className="uil uil-file-download-alt button_icon"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Social = () => {
+    return(
+        <div className="home_social">
+            <a href="https://github.com/moru628/NOMNOM.git" className="home_social-icon" target="_blank">
+                <i class="uil uil-github"></i>
+            </a>
+            <a href="#" className="home_social-icon" target="_blank">
+                <i class="uil uil-laughing"></i>
+            </a>
+            <a href="#" className="home_social-icon" target="_blank">
+                <i class="uil uil-smile-beam"></i>
+            </a>
+            <a href="#" className="home_social-icon" target="_blank">
+                <i class="uil uil-thumbs-up"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
